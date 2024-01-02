9b698a7ea3eb6ff61cc75e8b08ac2047766bb958
Louan B <louanbelicaud63@gmail.com>
2024-01-02 22:52:57 +0100

    ✨Add login, register and logout routes

--- api/app.py
+++ api/app.py
@@ -1,16 +1,25 @@
+import datetime
 import os
 
 import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, JWTManager, get_jti, jwt_required, get_jwt
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
 CORS(app)
 
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')  # Changez ceci avec votre propre clé secrète
+
+jwt = JWTManager(app)
+
+
 
 @app.route('/boards/<int:board_id>', methods=['GET'])
+@jwt_required()
 def get_board(board_id):
     load_dotenv()
     connection_string = os.getenv('DATABASE_URL')
@@ -62,7 +71,9 @@ def get_board(board_id):
 
     return jsonify(board_data)
 
+
 @app.route('/boards/delete/<int:board_id>', methods=['DELETE'])
+@jwt_required()
 def delete_board(board_id):
     load_dotenv()
     connection_string = os.getenv('DATABASE_URL')
@@ -90,6 +101,7 @@ def delete_board(board_id):
 
 
 @app.route('/partialBoards', methods=['GET'])
+@jwt_required()
 def get_partial_board_list():
     load_dotenv()
     connection_string = os.getenv('DATABASE_URL')
@@ -124,6 +136,7 @@ def get_partial_board_list():
 
 
 @app.route('/board/create', methods=['POST'])
+@jwt_required()
 def create_board():
     load_dotenv()
     connection_string = os.getenv('DATABASE_URL')
@@ -166,6 +179,7 @@ def create_board():
 
 
 @app.route('/board/update-participants/<int:board_id>', methods=['PUT'])
+@jwt_required()
 def update_board(board_id):
     load_dotenv()
     connection_string = os.getenv('DATABASE_URL')
@@ -199,6 +213,7 @@ def update_board(board_id):
 
 
 @app.route('/board/add-participant/<int:board_id>', methods=['POST'])
+@jwt_required()
 def add_participant(board_id):
     load_dotenv()
     connection_string = os.getenv('DATABASE_URL')
@@ -234,6 +249,7 @@ def add_participant(board_id):
 
 
 @app.route('/board/delete-participant/<int:board_id>', methods=['DELETE'])
+@jwt_required()
 def delete_item(board_id):
     load_dotenv()
     connection_string = os.getenv('DATABASE_URL')
@@ -270,6 +286,140 @@ def delete_item(board_id):
     conn.close()
 
     return jsonify({"message": "Item successfully deleted with ID: {}".format(deleted_item_id)})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    print(email, password)
+    # Vérifie les identifiants de l'utilisateur
+    if verify_user(email, password):
+        access_token = create_access_token(identity=email, expires_delta=datetime.timedelta(days=1))
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "Mauvais nom d'utilisateur ou mot de passe"}), 401
+
+
+def verify_user(email, password):
+    load_dotenv()
+    connection_string = os.getenv('DATABASE_URL')
+
+    conn = psycopg2.connect(connection_string)
+
+    try:
+        cur = conn.cursor()
+
+        cur.execute("SELECT password FROM users WHERE email = %s", (email,))
+
+        user_data = cur.fetchone()
+        cur.close()
+
+        if user_data is not None:
+            user_password_hash = user_data[0]
+            print(user_password_hash, password)
+            return check_password_hash(user_password_hash, password)
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return False
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    email: str = request.json.get('email')
+    password: str = request.json.get('password')
+
+    # Vérifier si l'email ou le mot de passe n'est pas fourni
+    if not email or not password:
+        return jsonify({"msg": "Email and password are required"}), 400
+
+    # Connexion à la base de données
+    load_dotenv()
+    connection_string = os.getenv('DATABASE_URL')
+    conn = psycopg2.connect(connection_string)
+
+    try:
+        cur = conn.cursor()
+
+        # Vérifier si l'utilisateur existe déjà
+        cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+        if cur.fetchone():
+            return jsonify({"msg": "Email already used"}), 409
+
+        # Hacher le mot de passe avant de le stocker
+        hashed_password = generate_password_hash(password, 'pbkdf2')
+
+        # Insérer le nouvel utilisateur dans la base de données
+        cur.execute("INSERT INTO users (email, password) VALUES (%s, %s)", (email, hashed_password))
+        conn.commit()
+
+        return jsonify({"msg": "Successfully registered the user"}), 201
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return jsonify({"msg": "Error while registering user"}), 500
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(jwt_header, jwt_payload):
+    jti = jwt_payload['jti']
+
+    try:
+        conn = psycopg2.connect(os.getenv('DATABASE_URL'))
+        cur = conn.cursor()
+
+        cur.execute("SELECT jti FROM blacklist WHERE jti = %s", (jti,))
+        token_in_blacklist = cur.fetchone()
+
+        return token_in_blacklist is not None
+
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        # In case of an error, consider the JWT as blacklisted for safety reasons
+        return True
+
+    finally:
+        cur.close()
+        conn.close()
+
+
+@app.route('/logout', methods=['POST'])
+@jwt_required()
+def logout():
+    current_jwt = get_jwt()
+
+    jti = current_jwt['jti']
+
+    expires_at = datetime.datetime.now() + datetime.timedelta(days=1)
+
+    try:
+        conn = psycopg2.connect(os.getenv('DATABASE_URL'))
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO blacklist (jti, expires_at) VALUES (%s, %s)", (jti, expires_at))
+        conn.commit()
+
+        return jsonify({"msg": "Successfully logged out"}), 200
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return jsonify({"msg": "Error while login out"}), 500
+
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
 
 
 if __name__ == '__main__':
